5b9a2026c698fca7e5073fc1fe380623d81fb34b
listenToRipley <natalie.m.kendrick@gmail.com>
2023-09-20 22:05:50 -0400

    map list

--- lesson1/RNCourse/App.js
+++ lesson1/RNCourse/App.js
@@ -1,16 +1,38 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [goalText, setGoalText] = useState('');
+  const [goalsList, setGoalsList] = useState([]);
+
+
+
+  function goalInputHandle(goalText) {
+    console.log(goalText);
+    setGoalText(goalText);
+  };
+
+  function addGoalHandler() {
+    console.log('click');
+    setGoalsList([
+      ...goalsList,
+      goalText,
+    ]);
+    console.log(goalText);
+    console.log(goalsList);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput 
           style={styles.textInput}
-          placeholder='Your course goal!' />
-        <Button title='Add Goal'/>
+          placeholder='Your course goal!'
+          onChangeText={goalInputHandle} />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of Goals</Text>
+        {goalsList.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
