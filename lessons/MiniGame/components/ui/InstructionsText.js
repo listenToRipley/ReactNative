e401b381c7217ea35537b673efcd7e746ed79810
listenToRipley <natalie.m.kendrick@gmail.com>
2024-03-03 19:13:33 -0500

    styling change

--- lessons/MiniGame/components/ui/InstructionsText.js
+++ lessons/MiniGame/components/ui/InstructionsText.js
@@ -7,6 +7,7 @@ export default function InstructionText ({children, style}) {
 
 const styles = StyleSheet.create({
   instructions: {
+    fontFamily: 'open-sans',
     color: Colors.accent500,
     fontSize: 24
   },
